d3a7d8d4835a7d75fec73669fb46e08f0d95eca0
Jossef Harush <jossef12@gmail.com>
2015-03-10 21:32:51 +0200

    fixed .gitignore

---
+++ config.js
@@ -0,0 +1,10 @@
+
+(function () {
+
+    exports.country = 'israel';
+    exports.city = 'tel aviv';
+    exports.floor = 1;
+    exports.scoreThreshold = 0.3;
+    exports.clockSentence = '17:30';
+
+})();
